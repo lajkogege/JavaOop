ce7823e15cd55c9a45aebdcfa2e162510b90e493
Lajkó Gege <127200655+lajkogege@users.noreply.github.com>
2024-04-17 11:01:53 +0200

    kesz

---
+++ src/osztalyokgyakorlas/Osztalyokgyakorlas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package osztalyokgyakorlas;
+
+/**
+ *
+ * @author LajkóGergő(SZOFT_202
+ */
+public class Osztalyokgyakorlas {
+
+    public static void main(String[] args) {
+      Ruha elsoRuha= new Ruha();
+      elsoRuha.kategoria="pulover";
+      elsoRuha.meret=32;
+      elsoRuha.szin="zöld";
+      elsoRuha.kiir();
+    }
+    
+}
---
+++ src/osztalyokgyakorlas/Ruha.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package osztalyokgyakorlas;
+
+/**
+ *
+ * @author LajkóGergő(SZOFT_202
+ */
+public class Ruha {
+    String szin;
+    String kategoria;
+    int meret;
+    void kiir(){
+        System.out.println("Szép ruha");
+    }
+    
+}
